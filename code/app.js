67ea717e8cf7533f88232985805e50af54e718de
Brian Ehmann <brian.ehmann@pearson.com>
2013-07-22 21:05:21 -0600

    starting slides

--- code/app.js
+++ code/app.js
@@ -1,7 +1,8 @@
 
-var push_pull = require('./lib/push-pull.js'),
-    req_res   = require('./lib/req-res.js'),
-    pub_sub   = require('./lib/pub-sub.js');
+var push_pull    = require('./lib/push-pull.js')
+  , req_res      = require('./lib/req-res.js')
+  , pub_sub      = require('./lib/pub-sub.js')
+  , load_balance = require('./lib/load-balance.js');
 
 
 //push_pull.server();
@@ -10,5 +11,8 @@ var push_pull = require('./lib/push-pull.js'),
 //req_res.server();
 //req_res.client();
 
-pub_sub.server();
-pub_sub.client();
+// pub_sub.client();
+// pub_sub.server();
+
+load_balance.broker();
+load_balance.worker();
