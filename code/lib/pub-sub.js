3a038f10c7ae6997cbb056be2d676dc0e2fda3ea
Brian Ehmann <behmann@gmail.com>
2013-07-23 22:48:11 -0600

    slides

--- code/lib/pub-sub.js
+++ code/lib/pub-sub.js
@@ -9,28 +9,28 @@
 'use strict';
 
 var zmq = require('zmq')
-  , server_socket = zmq.socket('pub')
-  , client_socket = zmq.socket('sub')
+  , pub_socket = zmq.socket('pub')
+  , sub_socket = zmq.socket('sub')
   , filter = 'pubsub';
 
 
-exports.server = function() {
-  server_socket.connect('tcp://127.0.0.1:3000');
+exports.pub = function() {
+  pub_socket.connect('tcp://127.0.0.1:3000');
   console.log('Producer bound to port 3000');
 
   setInterval(function() {
     console.log("sending work");
-    server_socket.send("pubsub work");
-    server_socket.send("bogus work");
+    pub_socket.send("pubsub work");
+    pub_socket.send("bogus work");
   }, 500);
 };
 
-exports.client = function() {
-  client_socket.subscribe(filter);
-  client_socket.bind('tcp://127.0.0.1:3000', function() {
+exports.sub = function() {
+  sub_socket.subscribe(filter);
+  sub_socket.bind('tcp://127.0.0.1:3000', function() {
     console.log('Worker connected to port 3000');
 
-    client_socket.on('message', function(msg) {
+    sub_socket.on('message', function(msg) {
       console.log('work: %s', msg.toString());
     });
   });
