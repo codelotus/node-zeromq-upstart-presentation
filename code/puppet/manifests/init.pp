f340504896086617398d5a8de5329600c2bc590f
Brian Ehmann <behmann@gmail.com>
2013-07-27 14:31:27 -0600

    fixed pub_sub upstart config files

--- code/puppet/manifests/init.pp
+++ code/puppet/manifests/init.pp
@@ -2,15 +2,29 @@
 include base
 include zeromq
 include nodejs
+include pub_sub_proxy
 
 Class['base']   ->
 Class['zeromq'] ->
-Class['nodejs']
+Class['nodejs'] ->
+Class['pub_sub_proxy']
 
 class base {
   # Needed to resolve misleading "mount files" error
   file { "/etc/puppet/files":
     ensure => directory,
+  }
+
+  package {'curl':
+    ensure => installed,
+  }
+
+  package {'finger':
+    ensure => installed,
+  }
+
+  user { 'node':
+    ensure => present,
   }
 }
 
--- code/puppet/modules/pub_sub_proxy/manifests/init.pp
+++ code/puppet/modules/pub_sub_proxy/manifests/init.pp
@@ -1,12 +1,30 @@
 
 class pub_sub_proxy {
 
+  file { "/etc/init/pub_sub_frontend.conf":
+    ensure => present,
+    source => "puppet:///modules/pub_sub_proxy/pub_sub_frontend.conf",
+    owner  => root,
+    group  => root,
+    mode   => 644,
+  }
 
-  exec { "npm-frontend":
-    cwd     => "/vagrant/pub_sub_proxy/frontend"
-    command => "npm install",
-    path    => ["/usr/bin", "/usr/sbin"],
-    creates => "/vagrant/pub_sub_proxy/frontend",
+
+  file { "/etc/init/pub_sub_proxy.conf":
+    ensure => present,
+    source => "puppet:///modules/pub_sub_proxy/pub_sub_proxy.conf",
+    owner  => root,
+    group  => root,
+    mode   => 644,
+  }
+
+
+  file { "/etc/init/pub_sub_worker.conf":
+    ensure => present,
+    source => "puppet:///modules/pub_sub_proxy/pub_sub_worker.conf",
+    owner  => root,
+    group  => root,
+    mode   => 644,
   }
 
 }
